8ca80c6113afd011dc1068c18fef56ed41ca22e5
hiroyamaguch <hiroyuki_yamaguchi@hotmail.com>
2021-04-14 14:49:32 -0300

    Updating Packages

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.0.3.2:3333",
+  baseURL: "http://10.0.3.2:3333", // Emulador
+  // baseURL: "http://10.0.0.21:3333", Dispositivo remoto
 });
 
 export default api;
